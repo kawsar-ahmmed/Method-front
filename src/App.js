c085798278433e38f5fe24d343307b653808572d
kawsar-ahmmed <kawsarahmmedr@gmail.com>
2023-06-10 08:14:06 +0600

    Backend Conection Initial Setup

--- src/App.js
+++ src/App.js
@@ -1,23 +1,56 @@
+import { useEffect, useState } from 'react';
 import logo from './logo.svg';
-import './App.css';
+
 
 function App() {
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:5001/users')
+      .then(res => res.json())
+      .then(data => setUsers(data))
+  }, [])
+
+
+  const handleFormButton = event => {
+    event.preventDefault();
+    const name = event.target.name.value;
+    const email = event.target.email.value;
+    const user = { name, email };
+
+    // POST data to the server
+    fetch('http://localhost:5001/users', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      });
+
+  };
+
+
+  console.log(users)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form action="" onSubmit={handleFormButton}>
+        <input type="text" name="name" placeholder='name' />
+        <input type="email" name="email" placeholder='Email' />
+        <button>Add User</button>
+      </form>
+      {
+        users.map(user =>
+
+          <div key={user.id}>
+            <ul>
+              <li>Name: {user.title} Email: {user.email} ID: {user.id} </li>
+            </ul>
+          </div>
+        )
+      }
     </div>
   );
 }
